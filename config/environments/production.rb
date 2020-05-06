0466cb7a3040c696d0c82007b9dd8c2f536ad08c
Randy Tozuka <kazm1208@me.com>
2020-05-06 16:27:01 +0900

    update the heroku user name and password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app170194787@heroku.com'],
+    :password       => ENV['srcyyuon7050'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
