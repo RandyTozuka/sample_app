7d6b5ec77529be7bb8111d573d68def83b700ce1
Randy Tozuka <kazm1208@me.com>
2020-05-15 18:37:38 +0900

    End up this training

--- app/models/user.rb
+++ app/models/user.rb
@@ -100,7 +100,7 @@ class User < ApplicationRecord
    def feed
      following_ids = "SELECT followed_id FROM relationships
                      WHERE follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
+     Micropost.where("user_id IN (#{following_ids})
                      OR user_id = :user_id", user_id: id)
    end
 
